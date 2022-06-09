8120c5bc8fcd7d199b3bb3d0af2765308094499f
Vincensius Seno Aji Pradhana <76866941+sen2402@users.noreply.github.com>
2022-06-09 11:36:26 +0700

    Add --noconfirm parameter

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@ cd $HOME
 
 echo Cloning Sources
 git clone https://ElectroPerf:$GITHUB_TOKEN@github.com/aospa-sdm660/android_kernel_asus_sdm660 -b sapphire-standalone --depth=1 $HOME/kernel
-sudo pacman -S wget
+sudo pacman -S wget --noconfirm
 echo Push Script
 cd $HOME/kernel
 wget https://raw.githubusercontent.com/ElectroPerf/myscripts/X01BD/asus.sh
