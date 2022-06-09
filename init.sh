51faca028bbbf179a06b142ac0e32c70a617fb8f
Vincensius Seno Aji Pradhana <76866941+sen2402@users.noreply.github.com>
2022-06-09 11:31:50 +0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@ cd $HOME
 
 echo Cloning Sources
 git clone https://ElectroPerf:$GITHUB_TOKEN@github.com/aospa-sdm660/android_kernel_asus_sdm660 -b sapphire-standalone --depth=1 $HOME/kernel
-sudo apt-get install wget
+sudo pacman -S wget
 echo Push Script
 cd $HOME/kernel
 wget https://raw.githubusercontent.com/ElectroPerf/myscripts/X01BD/asus.sh
